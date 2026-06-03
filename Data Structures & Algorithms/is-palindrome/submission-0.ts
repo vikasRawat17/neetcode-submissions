b761d51cfab7002e65023e415dae0ba19e9cb8c4
VIKAS SINGH RAWAT <168171683+vikasRawat17@users.noreply.github.com>
2026-06-03 13:25:14 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+         s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+        let left=0;
+        let right=s.length-1;
+
+        while(left<right){
+           if(s[left]!==s[right]){
+            return false;
+           }
+           left++;
+           right--;
+        }
+        return true;
+    }
+}
