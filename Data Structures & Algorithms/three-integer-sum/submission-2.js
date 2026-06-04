16f326d1ca66313d9260552d06e34c5bc32fa4a0
VIKAS SINGH RAWAT <168171683+vikasRawat17@users.noreply.github.com>
2026-06-04 11:28:55 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]}
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b)=>a-b);
+        const result = [];
+          
+      for(let i=0;i<nums.length;i++){
+
+        if(i > 0 && nums[i] === nums[i - 1]) {
+        continue;
+    }
+        let right=nums.length-1;
+        let left=i+1;
+        while(left<right){
+            let sum =nums[i]+nums[left]+nums[right];
+
+            if(sum==0){
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+            else if(sum>0){
+                right--
+            }else{
+                 left++;
+            }
+            
+               
+            
+        }
+      }
+          return result;
+
+    }
+}
